2d2f064d8bb6fbc0485443300c25c4fffbf9044f
23CSA HARIPRASANNAKUMAR R P <139534251+1U23CS037HARIPRASANNAKUMAR@users.noreply.github.com>
2025-02-14 04:38:20 +0000

    submit

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,4 +1,4 @@
-import java.util.*;
+ import java.util.*;
 public class ArrayStack{	
 protected int capacity;
 protected int[] stack;
@@ -11,21 +11,32 @@ public int size()
 {
     return (top+1);
 }
-public void push() 
+public void push(int data) 
 {
-// TYPE YOUR CODE HERE
+  if(isFull()) {
+    System.out.println("Stack is Overflow.Not possible to insert in Full stack");
+  }
+  else{
+    stack[++top]=data;
+    System.out.println("Element is inserted");
+  }
 }
 public int pop() 
 {
-// TYPE YOUR CODE HERE
+  if(isEmpty()){
+    System.out.println("Stack is Underflow.No elements to be popped in Empty Stack");
+    return-1;
+  } else {
+    return stack[top--];
+  }
 }
 public boolean isFull()
 {
-// TYPE YOUR CODE HERE
+ return size()==capacity;
 }
 public boolean isEmpty()
 {
-    // TYPE YOUR CODE HERE
+return top<0;
 }
 public void display()
 {
